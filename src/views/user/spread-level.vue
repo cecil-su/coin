c1e50aad21584bcf53bff636605015898750308e
cecil-su <cecil_su@163.com>
2017-12-26 20:57:39 +0800

    修改快速升级按钮限制

--- src/views/user/spread-level.vue
+++ src/views/user/spread-level.vue
@@ -4,21 +4,21 @@
             <div class="flex between level">
                 <div>渠道商</div>
                 <div style="color: red;">支付200MCC</div>
-                <div><a class="btn" v-if="user.vip_level < 3" @click="handleLevelUpdate">升级</a></div>
+                <div><a class="btn"  @click="handleLevelUpdate" :class="[user.vip_level > 2 ? 'disabled':'']">升级</a></div>
             </div>
         </div>
         <div class="coin-user-spread-item">
             <div class="flex between level">
                 <div>代理商</div>
                 <div style="color: red;">支付2000MCC</div>
-                <div><a class="btn" v-if="user.vip_level < 4" @click="handleLevelUpdate">升级</a></div>
+                <div><a class="btn" @click="handleLevelUpdate" :class="[user.vip_level > 3 ? 'disabled':'']">升级</a></div>
             </div>
         </div>
         <div class="coin-user-spread-item">
             <div class="flex between level">
                 <div>合伙人</div>
                 <div style="color: red;">支付5000MCC</div>
-                <div><a class="btn" v-if="user.vip_level < 5"  @click="handleLevelUpdate">升级</a></div>
+                <div><a class="btn"  @click="handleLevelUpdate" :class="[user.vip_level > 4 ? 'disabled':'']">升级</a></div>
             </div>
         </div>
         <div class="coin-user-spread-mask" v-show="alertVisible" @click="alertVisible = false">
@@ -34,7 +34,7 @@
 </template>
 
 <script>
-    import {initUserSpreadLevel, initUser} from '../../vuex/actions'
+    import {initUserSpreadLevel} from '../../vuex/actions'
     export default {
         data () {
             return {
@@ -44,16 +44,14 @@
         vuex: {
             actions: {
                 initUserSpreadLevel,
-                initUser,
             },
             getters: {
-                user: (state) => state.user,
+                user: (state) => state.user
             }
         },
         route: {
             data () {
                 this.initUserSpreadLevel()
-                this.initUser()
             }
         },
         methods: {
